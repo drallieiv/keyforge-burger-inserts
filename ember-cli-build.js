b1e4082d82d47c8f55683b4f33c24e90b9c8a787
Adrien Nguyen <knabyss@gmail.com>
2020-04-01 03:30:03 +0200

    remove fingerprint on images

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,11 +2,14 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
     'ember-cli-foundation-6-sass': {
       'foundationJs': 'all'
+    },
+    fingerprint: {
+      extensions: ['js', 'css']
     }
   });
 
